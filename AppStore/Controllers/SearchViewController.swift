458225d21cd3ee25c09102a3a005886bf54bdec9
nsemirdev <nsemirdev@gmail.com>
2023-02-25 17:30:30 +0300

    implement search controller

--- AppStore/Controllers/SearchViewController.swift
+++ AppStore/Controllers/SearchViewController.swift
@@ -12,29 +12,43 @@ final class SearchViewController: UICollectionViewController, UICollectionViewDe
     private static let cellID = "SearchViewController"
     private var results = [ApiResult]()
     
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private let enterSearchTermLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
     convenience init() {
         self.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupEmptyView()
+        setupSearchBar()
         setupCollectionView()
-        fetchApps()
     }
     
-    private func fetchApps() {
-        NetworkService.shared.fetchApps { result in
-            switch result {
-            case .success(let results):
-                DispatchQueue.main.async { [weak self] in
-                    guard let self else { return }
-                    self.results = results
-                    self.collectionView.reloadData()
-                }
-            case .failure(let failure):
-                print(failure)
-            }
-        }
+    private func setupEmptyView() {
+        view.addSubview(enterSearchTermLabel)
+        
+        NSLayoutConstraint.activate([
+            enterSearchTermLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            enterSearchTermLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            enterSearchTermLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            enterSearchTermLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
     }
     
     private func setupCollectionView() {
@@ -47,12 +61,34 @@ final class SearchViewController: UICollectionViewController, UICollectionViewDe
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        results.count
+        enterSearchTermLabel.isHidden = results.count != 0
+        return results.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchViewController.cellID, for: indexPath) as! SearchResultCell
         cell.appResult = results[indexPath.item]
         return cell
+    }
+    
+    var timer: Timer?
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+            NetworkService.shared.fetchApps(searchTerm: self.searchController.searchBar.text!) { result in
+                switch result {
+                case .success(let results):
+                    DispatchQueue.main.async {
+                        self.results = results
+                        self.collectionView.reloadData()
+                    }
+                case .failure(let failure):
+                    print(failure)
+                }
+            }
+        })
     }
 }
--- AppStore/Services/NetworkService.swift
+++ AppStore/Services/NetworkService.swift
@@ -11,8 +11,8 @@ final class NetworkService {
     public static let shared = NetworkService()
     private init() {}
     
-    public func fetchApps(completion: @escaping (Result<[ApiResult], Error>) -> Void) {
-        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+    public func fetchApps(searchTerm: String, completion: @escaping (Result<[ApiResult], Error>) -> Void) {
+        let urlString = "https://itunes.apple.com/search?term=\(searchTerm)&entity=software"
         let req = URLRequest(url: URL(string: urlString)!)
         
         URLSession.shared.dataTask(with: req) { data, response, error in
